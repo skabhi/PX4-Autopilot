e2185f6a12909121819621f67c07978d40f52d19
Abhishek Singh <anderson.singh@gmail.com>
2024-06-09 03:05:07 +0530

    added skabhi.sh

---
+++ skabhi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# git branch -r
+
+# git switch anubhav_quad
+
+# Add upstream remote
+git remote add upstream https://github.com/PX4/PX4-Autopilot.git
+
+# Fetch upstream changes
+git fetch upstream
+
+echo "Upstream remote added and fetched successfully."
+
